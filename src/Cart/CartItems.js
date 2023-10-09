c452e0c44a4af645662892e2e390ae9646b20693
arbaj0612 <junaid.786mulani@gmail.com>
2023-10-09 19:52:32 +0530

    css modified

--- src/Cart/CartItems.js
+++ src/Cart/CartItems.js
@@ -5,7 +5,7 @@ import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import "../Style/CartStyle.css"
 
 const CartItems = () => {
-	const cartArr = [0,2];
+	const cartArr = [0, 2];
 	return (
 		<>
 			<div className="row justify-content-center m-0">
@@ -13,67 +13,67 @@ const CartItems = () => {
 					<div className="card">
 						<div className="card-header bg-light p-3">
 							<div className="card-header-flex">
-									<h5 className="text-black m-0">Shopping Cart (01)</h5>
-									{
-									cartArr.length > 0 ? 
-									<Button variant="info" className="mt-0 btn btn-sm">
-										<FontAwesomeIcon icon={faTrashCan} /> Empty Cart
-									</Button> : ""
-                            		 }
+								<h5 className="text-black m-0">Shopping Cart (01)</h5>
+								{
+									cartArr.length > 0 ?
+										<Button variant="info" className="mt-0 btn btn-sm">
+											<FontAwesomeIcon icon={faTrashCan} /> Empty Cart
+										</Button> : ""
+								}
 							</div>
 						</div>
 						<div className="car-body p-0">
 							{
 								cartArr.length === 0 ? <table className="table cart-table mb-0">
-											<tbody>
-												<tr>
-													<td colSpan={6}>
-														<div className="cart-empty">
-															<i className="fa fa-shopping-cart"></i>
-															<p>Your Cart Is Empty</p>
-														</div>
-													</td>
-												</tr>
-											</tbody>
-								</table> : 
-								<table className="table cart-table mb-0 table-responsive-sm">
 									<tbody>
-										{
-											cartArr.map((data,index)=>{
-												return(
-													<>
-														<tr>
-															<td><div className="product-img"><img src="/logo192.png"/></div></td>
-															<td>
-																<div className="product-name">
-																	<p>Hemono Tablets - Support The Treatment Of Hemorrhoids</p>
-																	<p className="cart-item my-1">250g</p>
-																	<p>₹60 ₹120 (50.00% OFF)</p>
-																	<div>
-																		<button  className="prdct-delete"><FontAwesomeIcon icon={faTrashCan} /></button> <span>Remove</span>
+										<tr>
+											<td colSpan={6}>
+												<div className="cart-empty">
+													<i className="fa fa-shopping-cart"></i>
+													<p>Your Cart Is Empty</p>
+												</div>
+											</td>
+										</tr>
+									</tbody>
+								</table> :
+									<table className="table cart-table mb-0 table-responsive-sm">
+										<tbody>
+											{
+												cartArr.map((data, index) => {
+													return (
+														<>
+															<tr>
+																<td><div className="product-img"><img src="/logo192.png" /></div></td>
+																<td>
+																	<div className="product-name">
+																		<p>Hemono Tablets - Support The Treatment Of Hemorrhoids</p>
+																		<p className="cart-item move-to-top my-0"><small>250g</small></p>
+																		<p className="text-adjust"><b>₹60</b> <del>₹120</del> <small className="text-color-green">(50.00% OFF)</small></p>
+																		<div className="prdct-remove-btn">
+																			<button className="prdct-delete"><FontAwesomeIcon icon={faTrashCan} /></button> <span>Remove</span>
+																		</div>
 																	</div>
-																</div>
-															</td>
-															<td>
-																<div className="product-qty-continer">
+																</td>
+																<td>
+																	<div className="product-qty-continer">
 																		<button className="prdct-qty-btn" type="button">
 																			<i className="fa fa-minus"></i>
 																		</button>
-																			<input type="text" className="qty-input-box" value={1} disabled name="" id=""/>
+																		<input type="text" className="qty-input-box" value={1} disabled name="" id="" />
 																		<button className="prdct-qty-btn" type="button">
 																			<i className="fa fa-plus"></i>
 																		</button>
-																			
-																</div>
-															</td>
-														</tr>
-													</>
-												)
-											})
-										}
-									</tbody>
 
-								</table>
+																	</div>
+																</td>
+															</tr>
+														</>
+													)
+												})
+											}
+										</tbody>
+
+									</table>
 
 							}
 
